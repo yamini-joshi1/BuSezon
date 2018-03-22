605bd119b69099cb25c2f7bba53d96228455c67d
shriya <shri17ya@gmail.com>
2018-03-23 00:52:39 +0530

    shriya

--- app/src/main/java/com/example/mansi/busezon/MainActivity.java
+++ app/src/main/java/com/example/mansi/busezon/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mansi.busezon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button SignUp=(Button)findViewById(R.id.signup);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(getApplicationContext(), SigningUpActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
